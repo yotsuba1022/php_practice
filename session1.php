6f748271c2a7e04c9b711150ef2dbcca14a3344a
yotsuba1022 <yotsuba1022@gmail.com>
2017-04-15 22:03:41 +0800

    A simple way to manipulate session.

---
+++ session1.php
@@ -0,0 +1,23 @@
+<?php
+    /*
+    Start new or resume existing session, for more information, 
+    please refer to http://php.net/manual/en/function.session-start.php
+    */
+    session_start();
+    $_SESSION['greeting'] = "Que pa so!";
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <?php
+    
+    ?>
+    </body>
+
+    </html>
