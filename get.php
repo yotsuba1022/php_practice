e374a53ddfb388e3be5a6f695b8eaf0184df3718
yotsuba1022 <yotsuba1022@gmail.com>
2017-04-15 18:59:29 +0800

    get.php

---
+++ get.php
@@ -0,0 +1,22 @@
+<?php
+    print_r($_GET);
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <?php
+            $id = 10;
+            $button = "CLICK HERE";
+        ?>
+            <a href="get.php?id=<?php echo $id; ?>">
+                <?php echo $button; ?>
+            </a>
+    </body>
+
+    </html>
